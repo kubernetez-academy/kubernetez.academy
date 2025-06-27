5d32fca408861ffe9bb2e847d83cc1e9b53c6fbe
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-06-27 18:20:00 +0200

    add flake
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- _config.ts
+++ _config.ts
@@ -1,16 +1,7 @@
 import lume from "lume/mod.ts";
-import esbuild from "lume/plugins/esbuild.ts";
-import nav from "lume/plugins/nav.ts";
-import nunjucks from "lume/plugins/nunjucks.ts";
-import pug from "lume/plugins/pug.ts";
-import remark from "lume/plugins/remark.ts";
 
-const site = lume();
-
-site.use(esbuild());
-site.use(nav());
-site.use(nunjucks());
-site.use(pug());
-site.use(remark());
+const site = lume({
+  src: "src",
+});
 
 export default site;
